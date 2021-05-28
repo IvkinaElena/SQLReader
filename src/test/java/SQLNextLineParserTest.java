233a78153e615922d6d76efd1f474481b682c54e
IvkinaElena <sergeeva08em1@gmail.com>
2021-05-28 16:11:44 +0300

    Fix #4 and Fix #5

--- src/test/java/SQLNextLineParserTest.java
+++ src/test/java/SQLNextLineParserTest.java
@@ -3,13 +3,12 @@ import org.junit.jupiter.api.Test;
 public class SQLNextLineParserTest {
     @Test
     public void containsCreateTableTest() {
-        assert(SQLNextLineParser.statementContainsCreateTable("CREATE TABLE TEST"));
-        assert(SQLNextLineParser.statementContainsCreateTable("CREATE    TABLE     TEST"));
-        assert(SQLNextLineParser.statementContainsCreateTable("CREATE" + "/n"+ "TABLE TEST"));
-        assert(SQLNextLineParser.statementContainsCreateTable("create TABLE TEST"));
-        assert(SQLNextLineParser.statementContainsCreateTable("CREATE table TEST"));
-        assert(!SQLNextLineParser.statementContainsCreateTable("CREATE button TEST"));
-        assert(!SQLNextLineParser.statementContainsCreateTable("test test TEST"));
-
+        assert (SQLNextLineParser.statementContainsCreateTable("CREATE TABLE TEST"));
+        assert (SQLNextLineParser.statementContainsCreateTable("CREATE    TABLE     TEST"));
+        assert (SQLNextLineParser.statementContainsCreateTable("CREATE" + "/n" + "TABLE TEST"));
+        assert (SQLNextLineParser.statementContainsCreateTable("create TABLE TEST"));
+        assert (SQLNextLineParser.statementContainsCreateTable("CREATE table TEST"));
+        assert (!SQLNextLineParser.statementContainsCreateTable("CREATE button TEST"));
+        assert (!SQLNextLineParser.statementContainsCreateTable("test test TEST"));
     }
 }
