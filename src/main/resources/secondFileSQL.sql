9e6cea22c2b4f3e69d9491a6de59262947d73974
IvkinaElena <sergeeva08em1@gmail.com>
2021-06-20 15:46:08 +0300

    Update tests sqlFile

--- src/main/resources/secondFileSQL.sql
+++ src/main/resources/secondFileSQL.sql
@@ -1,7 +1,13 @@
-CREATE TABLE cinemas (
-                         id serial,
-                         name text,
-                         location text,
-                         date_test timestamp,
-                         datetime_test date
+CREATE TABLE cinemas
+(
+id
+serial,
+                         name
+                             text,
+                         location
+                             text,
+                         date_test
+                             timestamp,
+                         datetime_test
+                             date
 ) TABLESPACE diskvol1;
